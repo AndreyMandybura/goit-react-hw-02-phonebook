5a401d5b48b8e594c32eaa36cda1e2260d4ab080
AndreyMandybura <80692584+AndreyMandybura@users.noreply.github.com>
2021-11-18 17:46:38 +0200

    Initial

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+
+class App extends Component {
+
+  state = {
+    
+  };
+  
+  render() {
+    return (
+      <div>
+      </div>
+      );
+  }
+}
+
+export default App;
